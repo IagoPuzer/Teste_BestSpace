f74f5a30c83622b59f0c030560959f63ea500b1b
iagop <iagopuzer06@hotmail.com>
2024-02-16 17:01:20 -0300

    Ajustes no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaPhone } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import Logo from "../assets/img/Logo.png";
 
 export function Footer() {
   return (
-    <div className=" bg-cyan-800">
+    <div className=" bg-slate-800">
       <div className="grid lg:flex py-20 gap-y-10 justify-center items-center gap-x-24 border-b-2 border-neutral-500">
         <div className="mb-4 md:mb-0 flex flex-col justify-center items-center">
+          <img src={Logo} alt="Logotipo da Empresa" className="mb-2"></img>
           <span className="text-neutral-100 text-center">
-            Simplificando o universo jurídico
+            A 1º Inteligência Artifical <br /> aberta para expansão do varejo.
           </span>
         </div>
 
@@ -41,6 +44,14 @@ export function Footer() {
                 <FaPhone />
               </div>
               <span className="text-neutral-100">+55 11 4208-7186</span>
+            </li>
+            <li className="flex items-center gap-2">
+              <div className="bg-neutral-100 rounded-full w-10 h-10 flex justify-center items-center">
+                <IoMdMail />
+              </div>
+              <span className="text-neutral-100">
+                contato@bestspaces.com.br
+              </span>
             </li>
           </ul>
         </div>
