bc27bf1503acd1bce4d8018aada6ef22ef97be59
iagop <iagopuzer06@hotmail.com>
2024-02-16 15:48:36 -0300

    Criando o hero da page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
+import Banner from "../assets/banner-novo.png";
+
 export function Hero() {
   return (
-    <>
-      <h1>teste</h1>
-    </>
+    <div id="home" className="relative w-screen">
+      <div className="bg-cover bg-center h-[600px] 2xl:h-[800px] relative">
+        <img src={Banner} alt="" className="w-full h-full object-cover" />
+        <div className="absolute top-1/2 left-1/4 transform -translate-x-1/2 -translate-y-1/2 text-left text-neutral-1000 z-50">
+          <h1 className="text-3xl leading-10 md:text-5xl font-bold mb-4 md:leading-[60px]">
+            <span className="text-cyan-800">Best</span>
+            <span className="text-cyan-600">Spaces</span>
+          </h1>
+          <p className="text-lg md:text-xl font-medium mb-8 text-cyan-800">
+            A 1a plataforma aberta de IA para <br /> a busca do ponto comercial
+            perfeito!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
