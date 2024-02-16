91315a09fc8f05b1ba7629eb21e9a18d5e9f6cf4
iagop <iagopuzer06@hotmail.com>
2024-02-16 18:47:22 -0300

    Ajustes no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
+import { Form } from "./components/Form";
 // import { Btn } from "./components/Btn";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Hero />
-      <div className="mx-auto max-w-6xl my-12 space-y-6">
-        <form className="w-full">
-          <input
-            type="text"
-            placeholder="Busque em suas notas..."
-            className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
-          />
-        </form>
-
-        <div className="h-[1px] bg-slate-700" />
-
-        <div className="grid grid-cols-3 auto-rows-[250px] gap-6"></div>
+      <div id="Hero">
+        <Hero />
       </div>
-      <img src="./assets/banner-novo.png" alt="" className="z-20" />
+      <div id="Form">
+        <Form />
+      </div>
       {/* <Btn Text="teste" /> */}
-      <div id="Footer">
-        <Footer />
-      </div>
+
+      <Footer />
     </div>
   );
 }
